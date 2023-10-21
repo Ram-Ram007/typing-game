0becf2542e7fbe302986f9c8e57fdf6847fc006a
Ram-Ram007 <ramajayam2411@gmail.com>
2023-10-21 23:27:24 +0530

    added questions

--- app/app.js
+++ app/app.js
@@ -2,3 +2,19 @@ const main = document.querySelector(".main");
 const typeArea = document.querySelector(".typingArea");
 const btn = document.querySelector(".btn");
 
+const words = [
+  "A day in the life of programmer",
+  "What is JavaScript?",
+  "What is React?",
+  "What is Programming Language?",
+  "What's your name?",
+  "Where are you from?",
+  "This is just random word",
+  "What is Remix.js?",
+  "New Technologies",
+  "Is programming hard?",
+  "Why do you wanna become a programmer?",
+  "Which programming language you like the most?",
+  "What is Golang? and why do you wanna learn it?",
+  "What is CSS",
+];
